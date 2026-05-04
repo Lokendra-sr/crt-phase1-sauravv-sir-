c6ae0aa556a5714a4a136f96aeda69bcf8dd2120
lokendra singh <lokendrasingh@lokendras-MacBook-Air.local>
2026-05-04 10:09:13 +0530

    Initial commit

--- 3 no nested if else.cpp
+++ 3 no nested if else.cpp
@@ -4,15 +4,14 @@ int main ()
 {
     int a,b,c;
     cin>>a>>b>>c;
-    if(a>b && b>c){
-        std::cout << "a is the largest" <<a;
-        
+    if(a>=b && a>=c){
+        cout<<"a is the largest"<<a;
     }
-    if(b>a && a>c){
-        cout<<"b is the largest"<<b ;
+    else if(b>=a && b>=c){
+        cout<<"b is the largest"<<b;
     }
-        else{
-        cout<<"c is the largest"<<c;
+    else{
+        cout<<"c is the largest" <<c;
     }
     return 0;
 }
